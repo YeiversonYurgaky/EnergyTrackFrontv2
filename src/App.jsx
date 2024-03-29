23cdbd13504047f65b75e9092b493662fef46cd1
YeiversonYurgaky <yeiverson.yurgakyj@unac.edu.co>
2024-03-29 12:48:25 -0500

    :construction: Created route capabilities with react router dom.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import "./App.css";
-import NavbarPage from "./Pages/NavbarPage";
+import HomePage from "./Pages/HomePage";
+import LoginPage from './Pages/LoginPage';
 
 function App() {
-  return <NavbarPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      {/* <Route path="/register" element={<Register />} />
+      <Route path="/forgotpassword" element={<RecoverPassword />} />
+      <Route path="/dashboard" element={<NavbarPage />} />
+      <Route path="*" element={<PageNotFound />} /> */}
+  </Routes>
+  );
 }
 
 export default App;
